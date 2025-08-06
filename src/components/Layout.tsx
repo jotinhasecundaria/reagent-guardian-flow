2bb925e48553d94d17b37163662b360ab6bbfcfa
jota <jvfranco08@gmail.com>
2025-08-06 09:43:17 -0300

    a

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,16 +16,16 @@ export function Layout({ children }: LayoutProps) {
         <div className="flex-1 flex flex-col">
           {/* Header */}
           <header className="border-b bg-card/50 backdrop-blur supports-[backdrop-filter]:bg-card/50">
-            <div className="flex h-16 items-center justify-between px-6">
-              <div className="flex items-center gap-4">
+            <div className="flex h-16 items-center justify-between mx-2 md:mx-6 my-2">
+              <div className="flex items-center gap-2">
                 <SidebarTrigger />
                 <div>
-                  <h2 className="text-lg font-semibold text-foreground">Sistema de Controle de Reagentes</h2>
-                  <p className="text-sm text-muted-foreground">Gestão inteligente com QR codes</p>
+                  <h2 className="text-md md:text-lg font-semibold text-foreground">Sistema de Controle de Reagentes</h2>
+                  <p className="text-xs md:text-sm text-muted-foreground">Gestão inteligente com QR codes</p>
                 </div>
               </div>
               
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-0 md:gap-2 hidden md:inline">
                 <Button variant="ghost" size="icon">
                   <Bell className="h-4 w-4" />
                 </Button>
